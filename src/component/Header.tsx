4469eb48a1a7163e1ae2323f4249c11591df1e12
Adoilson Freitas <adoilsonfreitas@gmail.com>
2022-05-13 13:18:16 -0300

    Create header component

---
+++ src/component/Header.tsx
@@ -0,0 +1,9 @@
+import { Center, Image } from "@chakra-ui/react";
+
+export default function Header() {
+  return (
+    <Center h="100px">
+      <Image src="/images/Logo.svg" alt="Logo" />
+    </Center>
+  );
+}
