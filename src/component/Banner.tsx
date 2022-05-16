db440f33fb585157e37f1fad35928b2ec1666499
Adoilson Freitas <adoilsonfreitas@gmail.com>
2022-05-16 16:36:27 -0300

    Create banner component

---
+++ src/component/Banner.tsx
@@ -0,0 +1,9 @@
+import { Box, Image } from "@chakra-ui/react";
+
+export default function Banner() {
+  return (
+    <Box>
+      <Image src="/images/Banner.svg" alt="Banner" />
+    </Box>
+  );
+}
