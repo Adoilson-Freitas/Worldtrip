506113330f4bbc692d3609749f8534c49e06709c
Adoilson Freitas <adoilsonfreitas@gmail.com>
2022-05-19 15:05:59 -0300

    Fix import images

--- src/component/Banner.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Image } from "@chakra-ui/react";
-
-export default function Banner() {
-  return (
-    <Box>
-      <Image src="/images/Banner.svg" alt="Banner" />
-    </Box>
-  );
-}
--- src/component/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Image } from "@chakra-ui/react";
-
-export default function Header() {
-  return (
-    <Center h="100px">
-      <Image src="/images/Logo.svg" alt="Logo" />
-    </Center>
-  );
-}
